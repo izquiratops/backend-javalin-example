175e39e33b2f03c12b08e3107c982ef7a80bde01
izquiratops <jicasares94@gmail.com>
2022-05-20 12:12:41 +0200

    duck update

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,31 +7,35 @@ import org.eclipse.jetty.util.ssl.SslContextFactory;
 public class App {
     public static void main(String[] args) {
         Javalin app = Javalin.create(config -> {
-            config.server(() -> {
-                Server server = new Server();
-
-                /* TODO: wait this could be handled by nginx
-                // HTTPS
-                ServerConnector sslConnector = new ServerConnector(server, getSslContextFactory());
-                sslConnector.setPort(443);
-
-                // HTTP
-                ServerConnector connector = new ServerConnector(server);
-                connector.setPort(80);
-
-                server.setConnectors(new Connector[]{sslConnector, connector}); */
-                return server;
-            });
+            config.server(App::CreateSecureServer);
         }).start();
 
         app.routes(() -> {
             app.get("/", ctx -> ctx.result("Hello World"));
+            app.post("/upload", ProcessController.Upload);
         });
     }
+
+    private static Server CreateSecureServer() {
+        Server server = new Server();
+
+        /* // HTTPS
+        ServerConnector sslConnector = new ServerConnector(server, getSslContextFactory());
+        sslConnector.setPort(7071); */
+
+        // HTTP
+        ServerConnector connector = new ServerConnector(server);
+        connector.setPort(7070);
+
+        server.setConnectors(new Connector[]{connector});
+        // server.setConnectors(new Connector[]{sslConnector, connector});
+
+        return server;
+    }
+
     private static SslContextFactory getSslContextFactory() {
         SslContextFactory sslContextFactory = new SslContextFactory.Server();
-        // sslContextFactory.setKeyStorePath(System.getProperty("user.dir") + "/keystore.jks");
-        sslContextFactory.setKeyStorePath("/usr/app/certs/keystore.jks");
+        sslContextFactory.setKeyStorePath(System.getProperty("user.dir") + "/keystore.jks");
         sslContextFactory.setKeyStorePassword("rebotado");
         return sslContextFactory;
     }
