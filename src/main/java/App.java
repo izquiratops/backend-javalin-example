04b316fd794c767e41ab0279cde21ec96fb54a9e
izquiratops <jicasares94@gmail.com>
2022-05-24 10:37:28 +0200

    sdgsgdsg

---
+++ src/main/java/App.java
@@ -0,0 +1,42 @@
+import io.javalin.Javalin;
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
+
+public class App {
+    public static void main(String[] args) {
+        Javalin app = Javalin.create(config -> {
+            config.server(App::CreateSecureServer);
+        }).start();
+
+        app.routes(() -> {
+            app.get("/", ctx -> ctx.result("Hello World"));
+            app.post("/upload", ProcessController.Upload);
+        });
+    }
+
+    private static Server CreateSecureServer() {
+        Server server = new Server();
+
+        // HTTPS
+        ServerConnector sslConnector = new ServerConnector(server, getSslContextFactory());
+        sslConnector.setPort(443);
+
+        // HTTP
+        ServerConnector connector = new ServerConnector(server);
+        connector.setPort(80);
+
+        // server.setConnectors(new Connector[]{connector});
+        server.setConnectors(new Connector[]{sslConnector, connector});
+
+        return server;
+    }
+
+    private static SslContextFactory getSslContextFactory() {
+        SslContextFactory sslContextFactory = new SslContextFactory.Server();
+        sslContextFactory.setKeyStorePath(System.getProperty("user.dir") + "/certs/keystore.jks");
+        sslContextFactory.setKeyStorePassword(System.getenv().get("KEYSTORE_PASSWORD"));
+        return sslContextFactory;
+    }
+}
---
+++ src/main/java/ProcessController.java
@@ -0,0 +1,44 @@
+import io.javalin.http.Handler;
+
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.http.Part;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Collection;
+
+public class ProcessController {
+
+    public static String RECEIVED_DATA_PATH = System.getProperty("user.dir") + "/received_data";
+
+    public static Handler Upload = ctx -> {
+        ctx.req.setAttribute("org.eclipse.jetty.multipartConfig", new MultipartConfigElement("/temp"));
+        final Collection<Part> parts = ctx.req.getParts();
+
+        int index = 0;
+        for (Part part : parts) {
+            System.out.println("Part " + (index++));
+            System.out.println("Name: " + part.getName());
+            System.out.println("Size: " + part.getSize() + " bytes");
+            System.out.println("Filename: " + part.getSubmittedFileName());
+            System.out.println("----------------------------------------------------");
+        }
+
+        final String description = ctx.req.getParameter("description");
+        System.out.println("Description: " + description);
+
+        final Part uploadedFile = ctx.req.getPart("file");
+        final Path out = Paths.get(RECEIVED_DATA_PATH + "/" + uploadedFile.getSubmittedFileName());
+
+        try (final InputStream in = uploadedFile.getInputStream()) {
+            Files.copy(in, out, StandardCopyOption.REPLACE_EXISTING);
+            uploadedFile.delete();
+        }
+
+        System.out.println("------------------ End of request ------------------");
+        ctx.result("File Saved");
+    };
+
+}
