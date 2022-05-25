5722ddf6542da11f526fdec69ffa56d69d0a441f
Jordi Izquierdo <hi@izquiratops.dev>
2022-05-25 11:29:18 +0200

    woo

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,10 +1,14 @@
 import io.javalin.Javalin;
+import org.apache.logging.log4j.core.util.IOUtils;
 import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
 
-import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class App {
     public static void main(String[] args) {
@@ -36,10 +40,17 @@ public class App {
     }
 
     private static SslContextFactory getSslContextFactory() {
+        final String keystorePassword;
+        try {
+            keystorePassword = new String(Files.readAllBytes(Path.of("/run/secrets/keystore_password")));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         SslContextFactory sslContextFactory = new SslContextFactory.Server();
-        System.out.println("-------> " + System.getenv().get("KEYSTORE_PASSWORD"));
-        sslContextFactory.setKeyStorePath("/webserver/certs/keystore.jks");
-        sslContextFactory.setKeyStorePassword(System.getenv().get("KEYSTORE_PASSWORD"));
+        sslContextFactory.setKeyStorePath("/run/secrets/keystore_content");
+        sslContextFactory.setKeyStorePassword(keystorePassword);
+
         return sslContextFactory;
     }
 }
